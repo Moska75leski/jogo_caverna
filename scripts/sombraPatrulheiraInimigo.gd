e523f9f118c5b6cadfeee2507d8ff248e78476b6
Daniel urbaneki dos santos <daniel_urba@hotmail.com>
2024-10-29 21:59:42 -0300

    Adicionando vida aos inimigos e combates, e começando a adicionar mais inimigos e todos os mapas

--- scripts/sombraPatrulheiraInimigo.gd
+++ scripts/sombraPatrulheiraInimigo.gd
@@ -4,25 +4,36 @@ extends CharacterBody2D
 @export var detection_range = 100  # Alcance para detectar o jogador
 @export var patrol_points = []  # Lista de vetores para os pontos de patrulha
 @export var damage = 10
+@export var death_animation_duration = 1
+@export var knockback_distance = 50  # Distância do recuo
+@export var knockback_duration = 0.1
 
+var life = 100
 var player = null
 var current_patrol_index = 0
 var is_chasing_player = false
 var is_collision_player = false
+var dead = false
+
+var knockback_timer = 0.0
+var knockback_velocity = Vector2.ZERO
 
 func _ready() -> void:
 	$RayCast2D.enabled = true
-	player = get_node("/root/Level1/Player")
+	var players = get_tree().get_nodes_in_group("Player")
+	if players.size() > 0:
+		player = players[0]
 
 func _process(delta: float) -> void:
-	if is_chasing_player:
-		seek_player(delta)
-	else:
-		patrol(delta)
-	
-	update_animation()
-	check_for_player()
-	update_raycast_direction()
+	if not dead:
+		if is_chasing_player:
+			seek_player(delta)
+		else:
+			patrol(delta)
+			
+		update_animation()
+		check_for_player()
+		update_raycast_direction()
 
 func _on_body_entered(body):
 	if body == player:
@@ -44,18 +55,21 @@ func update_animation() -> void:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	
+	if is_collision_player:
+		$AnimatedSprite2D.animation = "atack"
 
-	if velocity.x > 0:
+	if velocity.x > 0 and not is_collision_player:
 		$AnimatedSprite2D.animation = "walk_right"
 		$AnimatedSprite2D.flip_h = false
-	elif velocity.x < 0:
+	elif velocity.x < 0 and not is_collision_player:
 		$AnimatedSprite2D.animation = "walk_left"
 		$AnimatedSprite2D.flip_h = true
-	elif velocity.y < 0:
+	elif velocity.y < 0 and not is_collision_player:
 		$AnimatedSprite2D.animation = "walk_up"
-	elif velocity.y > 0:
+	elif velocity.y > 0 and not is_collision_player:
 		$AnimatedSprite2D.animation = "walk_down"
-	else:
+	elif not is_collision_player:
 		$AnimatedSprite2D.animation = "idle"
 
 func patrol(delta: float) -> void:
@@ -88,3 +102,38 @@ func update_raycast_direction() -> void:
 	else:
 		if velocity.length() > 0:
 			$RayCast2D.target_position = velocity.normalized() * detection_range
+
+func take_damage(amount: int, direction: Vector2) -> void:
+	life -= amount
+	print("Vida do inimigo:", life)
+	if life <= 0:
+		dead = true
+		die()
+	else:
+		apply_knockback(direction)
+
+func apply_knockback(direction: Vector2) -> void:
+	knockback_velocity = direction.normalized() * -knockback_distance
+	knockback_timer = knockback_duration
+
+func _physics_process(delta: float) -> void:
+	if knockback_timer > 0:
+		velocity = knockback_velocity
+		knockback_timer -= delta
+	else:
+		velocity = Vector2.ZERO  # Parar o recuo após o término do tempo
+	move_and_slide()
+
+func die() -> void:
+	$AnimatedSprite2D.animation = "death"  # Nome da animação de morte
+	$AnimatedSprite2D.play()
+	
+	var death_timer = Timer.new()
+	death_timer.wait_time = death_animation_duration
+	death_timer.one_shot = true
+	death_timer.connect("timeout", _on_death_animation_complete)
+	add_child(death_timer)
+	death_timer.start()
+
+func _on_death_animation_complete() -> void:
+	queue_free()
